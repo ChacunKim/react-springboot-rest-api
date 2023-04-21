80b753c37f4a68b3ac7b93154893355daf8d2ead
Chaewoonii <cocodata12@gmail.com>
2023-04-21 17:44:14 +0900

    test: change configuration for test

--- coffee/src/main/java/com/cnu/coffee/configuration/SecurityConfig.java
+++ coffee/src/main/java/com/cnu/coffee/configuration/SecurityConfig.java
@@ -54,7 +54,6 @@ public class SecurityConfig {
                     .accessDeniedHandler(accessDeniedHandler())
                     .and()
                 .csrf().disable()
-
         ;
     return http.build();
     }
